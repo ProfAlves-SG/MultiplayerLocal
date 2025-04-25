5d177cada9cd5896306d102b0e5a6aec0c29ac7f
ProfAlves-SG <guilhermeaniceto.dev@gmail.com>
2025-04-25 18:17:10 -0300

    Atividade 8 finalizada

--- Assets/PackageSG/PlayerController.cs
+++ Assets/PackageSG/PlayerController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string horizontalInput = "Horizontal";
+    public string verticalInput = "Vertical";
 
-    // Update is called once per frame
+    public float speed = 5f;
+    public float rotationSpeed = 125f;
+
+    public Rigidbody rb;
+
     void Update()
     {
-        
+        float move = Input.GetAxis(horizontalInput);
+        float turn = Input.GetAxis(verticalInput);
+
+        Vector3 movement = transform.forward * move * speed * Time.deltaTime;
+        transform.Rotate(0, turn * rotationSpeed * Time.deltaTime, 0);
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
     }
 }
