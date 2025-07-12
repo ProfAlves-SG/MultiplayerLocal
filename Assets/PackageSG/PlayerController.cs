0e472cfc31f514b7844b1d539f48083f5cabf0a8
ProfAlves-SG <guilhermeaniceto.dev@gmail.com>
2025-07-12 08:50:28 -0300

    Finish project

--- Assets/PackageSG/PlayerController.cs
+++ Assets/PackageSG/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,75 +6,40 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // GD1 QUA
-
     public string horizontalInput = "Horizontal";
     public string verticalInput = "Vertical";
 
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 5f;
+    public float rotationSpeed = 125f;
 
     public Rigidbody rb;
 
     public float move;
     public float turn;
+    public int score;
 
-    private void Update()
-    {
+    public Text myScoreTxt;
+
+    void Update()
+    {   
         move = Input.GetAxis(verticalInput);
         turn = Input.GetAxis(horizontalInput);
-        
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = transform.forward * move * speed;
         transform.Rotate(0, turn * rotationSpeed * Time.deltaTime, 0);
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+         if(collision.gameObject.CompareTag("Coin"))
+         {
+             score++;
+             myScoreTxt.text = score.ToString();
+             Destroy(collision.gameObject);
+         }
+    }
 }
-
-
-
-
-
-
-
-
-
-//public string horizontalInput = "Horizontal";
-//public string verticalInput = "Vertical";
-
-//public float speed = 5f;
-//public float rotationSpeed = 125f;
-
-//public Rigidbody rb;
-
-//public float move;
-//public float turn;
-//public int score;
-
-//public Text myScoreTxt;
-
-//void Update()
-//{   
-//    move = Input.GetAxis(verticalInput);
-//    turn = Input.GetAxis(horizontalInput);
-//}
-
-//private void FixedUpdate()
-//{
-//    Vector3 movement = transform.forward * move * speed;
-//    transform.Rotate(0, turn * rotationSpeed * Time.deltaTime, 0);
-//    rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
-//}
-
-//private void OnCollisionEnter(Collision collision)
-//{
-//   if(collision.gameObject.CompareTag("Coin"))
-//   {
-//        score++;
-//        myScoreTxt.text = score.ToString();
-//        Destroy(collision.gameObject);
-//   }
-//}
